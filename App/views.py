717b61fabec9d025d4f02b65521a91e4b1a3225e
benjapob <124107286+benjapob@users.noreply.github.com>
2023-11-15 20:03:57 -0300

    pdf

--- App/views.py
+++ App/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render, redirect
 from .models import Reserva
 from . import forms
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+
 
 # Create your views here.
+
+def pdf(request, id):
+    reserva = Reserva.objects.get(idSolicitud=id)
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer, bottomup=0)
+
+    textob = p.beginText()
+    textob.setTextOrigin(inch, inch)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    if reserva.donante == 0:
+        donante = "No"
+    else:
+        donante = "Si"
+
+    lines = ["Reserva Nº"+str(id),
+             "",
+             "Nombre: "+reserva.nombre, 
+             "Edad: "+str(reserva.edad),
+             "Telefono: "+reserva.telefono,
+             "Fecha de Reserva: "+str(reserva.fechaReserva),
+             "Hora de Reserva: "+str(reserva.horaReserva),
+             "Cantidad de Hermanos: "+str(reserva.cantidadPersonas),
+             "Observaciones: "+ reserva.observaciones,
+             "Website: "+reserva.website,
+             "Email: "+reserva.email,
+             "Donante: "+donante,
+             "Estado Reserva: "+str(reserva.estadoReservaId),
+             "Tipo Solicitud: "+str(reserva.tipoSolicitudId)
+             ]
+    for line in lines:
+        textob.textLine(line)
+
+    # Close the PDF object cleanly, and we're done.
+    p.drawText(textob)
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename=f"reserva{id}.pdf")
 
 def agregar(request):
     reservas = Reserva.objects.all()
--- Sumativa2/urls.py
+++ Sumativa2/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from App.views import agregar
-from App.views import eliminar
-from App.views import actualizar
+from App.views import agregar, eliminar, actualizar, pdf
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -26,4 +24,5 @@ urlpatterns = [
     path('', agregar),
     path('eliminar/<int:id>', eliminar),
     path('actualizar/<int:id>', actualizar),
+    path('pdf/<int:id>', pdf, name='pdf'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
